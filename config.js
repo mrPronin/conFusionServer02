a3346cbd192896cb8eb870b07d52c1ed2d07039c
Alexander Pronin <pronin.alx@gmail.com>
2016-11-06 17:00:40 +0200

    Using OAuth with Passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '351486128576327',
+        clientSecret: '895101ae9d570b1845536c11eb0c2a18',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
